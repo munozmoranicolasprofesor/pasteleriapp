95220477cd377f4358c7d9e85cb2dcf0ee6adbca
munozmoranicolasprofesor <128549596+munozmoranicolasprofesor@users.noreply.github.com>
2025-09-23 18:02:37 -0300

    Agrega logo
    Se incorpora recurso logo.png y se modifica HomeScreen.kt para que muestre el logo

--- app/src/main/java/cl/munozmoranicolasprofesor/pasteleriapp/ui/HomeScreen.kt
+++ app/src/main/java/cl/munozmoranicolasprofesor/pasteleriapp/ui/HomeScreen.kt
@@ -34,7 +34,7 @@ fun HomeScreen(){
                 Text("Presioname")
             }
             Image(
-                painter = painterResource(id= R.drawable.ic_launcher_foreground),
+                painter = painterResource(id= R.drawable.logo),
                 contentDescription = "Logo App",
                 modifier = Modifier
                     .fillMaxWidth()
